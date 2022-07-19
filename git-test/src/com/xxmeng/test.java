d21f8def74942586e009d032b74c27aa3ea79e79
xxmeng <1347093749@qq.com>
2022-07-19 09:57:26 +0800

    hot-fix commit 1

---
+++ git-test/src/com/xxmeng/test.java
@@ -0,0 +1,16 @@
+package com.xxmeng;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author xxmeng
+ * @DATE 2022-07-19 9:56
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+        System.out.println("Hello,World!2");
+        System.out.println("Hello,World!3");
+        System.out.println("Hello,World!4");
+    }
+}
