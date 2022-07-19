c453049eff007fa52d020dd6bbdff2f70f3ad391
xxmeng <1347093749@qq.com>
2022-07-19 09:44:01 +0800

    first commit

---
+++ git-test/src/com/xxmeng/test.java
@@ -0,0 +1,12 @@
+package com.xxmeng;
+
+/**
+ * @author xxmeng
+ * @create 2022--07--19--8:56
+ */
+class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
